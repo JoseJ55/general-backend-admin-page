c23dbeb7e16d031201c8c761d9df88dfcbfc2b17
JoseJ55 <josejimenez826@gmail.com>
2023-03-25 23:45:56 -0700

    added animation to the info tabs so when first laoded they fade in

--- src/Components/MostUsedTab/MostUsedTab.jsx
+++ src/Components/MostUsedTab/MostUsedTab.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MostUsedTab.css';
 
 // SHows the endpoint that is most used
 function MostUsedTab() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
-    <div id="mostusedtab">
+    <div id="mostusedtab" style={{ transform: loading ? 'translateY(25%)' : 'translateY(0%)', opacity: loading ? 0 : 1 }}>
       <p id="mostusedtab-title">Most used</p>
       <p id="mostusedtab-content">/first</p>
     </div>
--- src/Components/UsedTodayTab/UsedTodayTab.jsx
+++ src/Components/UsedTodayTab/UsedTodayTab.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './UsedTodayTab.css';
 
 // Shows how many people used the endpoints today.
 function UsedTodayTab() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
-    <div id="usedtodaytab">
+    <div id="usedtodaytab" style={{ transform: loading ? 'translateY(40%)' : 'translateY(0%)', opacity: loading ? 0 : 1 }}>
       <p id="usedtodaytab-title">Amount of users who called an endpoint today</p>
       <p id="usedtodaytab-content">10</p>
     </div>
